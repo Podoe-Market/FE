11ab84cabe21d5a177355a76b184e36fc1723256
uwol-is-june <96686972+uwol-is-june@users.noreply.github.com>
2024-03-04 00:25:14 +0900

    0303

---
+++ main.js
@@ -0,0 +1,6 @@
+const toggleBtn = document.querySelector('.nav_togglebtn');
+const menu = document.querySelector('.nav_menu');
+
+toggleBtn.addEventListener('click', () => {
+  menu.classList.toggle('active');
+});
